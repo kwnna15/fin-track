222436006ba05cd54ca14dda7a8ccb3f919d9822
kwnna15 <137907037+kwnna15@users.noreply.github.com>
2025-03-18 22:59:29 +0100

    Always classify transactions

--- src/main/java/se/mycompany/fin/track/kafka/processor/TransactionProcessor.java
+++ src/main/java/se/mycompany/fin/track/kafka/processor/TransactionProcessor.java
@@ -2,6 +2,7 @@ package se.mycompany.fin.track.kafka.processor;
 
 import java.util.Optional;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import se.mycompany.fin.track.model.transaction.Transaction;
@@ -11,25 +12,26 @@ import se.mycompany.fin.track.repository.entity.ClassifiedTransactionEntity;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class TransactionProcessor {
 
-    ClassifiedTransactionRepository classifiedTransactionRepository;
+    private final ClassifiedTransactionRepository classifiedTransactionRepository;
 
     public void processTransaction(Transaction transaction) {
         log.info("Processing transaction {}", transaction);
-        Optional<TransactionClassification> maybeClassification = transaction.transactionClassification().stream()
+        TransactionClassification classification = transaction.transactionClassification().stream()
                 .map(value -> TransactionClassification.fromString(value.value()))
                 .flatMap(Optional::stream)
-                .findFirst();
+                .findFirst()
+                .orElse(TransactionClassification.UNCATEGORIZED);
 
-        if (maybeClassification.isPresent()) {
-            ClassifiedTransactionEntity entity = ClassifiedTransactionEntity.builder()
-                    .id(UUID.randomUUID())
-                    .transactionClassification(maybeClassification.get())
-                    .build();
+        ClassifiedTransactionEntity entity = ClassifiedTransactionEntity.builder()
+                .id(UUID.randomUUID())
+                .transactionClassification(classification)
+                .build();
 
-            classifiedTransactionRepository.save(entity);
-        }
+        classifiedTransactionRepository.save(entity);
+
         log.info("Processed transaction {}", transaction);
     }
 }
--- src/main/java/se/mycompany/fin/track/model/transaction/Transaction.java
+++ src/main/java/se/mycompany/fin/track/model/transaction/Transaction.java
@@ -3,9 +3,11 @@ package se.mycompany.fin.track.model.transaction;
 import java.time.Instant;
 import java.util.Set;
 import lombok.Builder;
+import lombok.extern.jackson.Jacksonized;
 import se.mycompany.fin.track.model.money.Money;
 
 @Builder
+@Jacksonized
 public record Transaction(
         TransactionId transactionId,
         Instant timestamp,
